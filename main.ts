c5f6e6beb944cdc1357bbaaf8e86d8f443fcd283
Tom Hansen <tomh@uwm.edu>
2024-04-02 05:50:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,127 @@
+input.onGesture(Gesture.LogoUp, function () {
+    if (sad) {
+        basic.pause(4000)
+        sad = 0
+    }
+    music.stopAllSounds()
+    images.createImage(`
+        . . . . .
+        # . # . .
+        . . . . #
+        # . . # .
+        . # # . .
+        `).showImage(0)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    if (sad) {
+        basic.pause(4000)
+    }
+    sad = 0
+    music.stopAllSounds()
+    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+    images.createImage(`
+        . . . . .
+        # . # . .
+        . . . . #
+        # . . # .
+        . # # . .
+        `).showImage(0)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    sad = 1
+    music.stopAllSounds()
+    images.createImage(`
+        . # . . .
+        . . . # .
+        . # # . .
+        # . . # .
+        . . . . #
+        `).showImage(0)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.stopAllSounds()
+    sad = 0
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.InBackground)
+    images.createImage(`
+        . # . . .
+        . . . . .
+        . # . # .
+        # . . # .
+        . # # . .
+        `).showImage(0, 100)
+    for (let index = 0; index < 4; index++) {
+        images.createImage(`
+            . . . . .
+            # . # . .
+            . . . . #
+            # . . # .
+            . # # . .
+            `).showImage(0, 100)
+        images.createImage(`
+            . # . . .
+            . . . . .
+            . # . # .
+            # . . # .
+            . # # . .
+            `).showImage(0, 100)
+    }
+    for (let index = 0; index < 4; index++) {
+        images.createImage(`
+            . . . . .
+            . # . # .
+            . . . . #
+            # . . # .
+            . # # . .
+            `).showImage(0, 50)
+        images.createImage(`
+            . . . . .
+            # . # . .
+            . . . . #
+            # . . # .
+            . # # . .
+            `).showImage(0, 200)
+    }
+    music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    music.stopAllSounds()
+    images.createImage(`
+        . . # # .
+        # . . . #
+        . . # . #
+        . . . . #
+        # # # # #
+        `).showImage(0)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.InBackground)
+})
+let sad = 0
+sad = 0
+images.createImage(`
+    . . . . .
+    # . # . .
+    . . . . #
+    # . . # .
+    . # # . .
+    `).showImage(0)
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+basic.forever(function () {
+    basic.pause(100)
+})
+control.inBackground(function () {
+    while (true) {
+        led.plot(4, 4)
+        basic.pause(80)
+        led.unplot(4, 4)
+        basic.pause(120)
+        led.plot(4, 4)
+        basic.pause(40)
+        led.unplot(4, 4)
+        basic.pause(600)
+        if (sad) {
+            basic.pause(1000)
+        }
+    }
+})
